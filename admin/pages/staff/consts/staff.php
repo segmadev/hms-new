eb9bfd3ab88dfebf327ecb9dd41cc98ee6fe34c7
segma.dev <serikioluwagbenga@gmail.com>
2025-12-13 17:46:19 +0100

    input renfder 2

--- admin/pages/staff/consts/staff.php
+++ admin/pages/staff/consts/staff.php
@@ -1,17 +1,24 @@
 <?php 
     $staff_from = [
+        "ID"=>["type"=>"hidden"],
         "name"=>[
             "value"=>"Segma",
-            "description"=>"Make sure to enter your first name and last name."
+            "description"=>"Make sure to enter your first name and last name.",
+            "class"=>"bg-sucess",
+            "placeholder"=>"John Doe",
+            "title"=>"Full Name"
         ],
         "email"=>[
             "input_type"=>"email"
         ],
         "role"=>[
             "type"=>"select",
-            "options"=>["main"=>"role 1", "admin"=>"role 2"],
+            "options"=>$d->options_list("roles", "ID", "name"),
         ],
-        "department"=>[],
+        "department"=>[
+            "type"=>"select",
+            "options"=>$d->options_list("departments", "department_id", "department_name"),
+        ],
         "contact"=>[
             "is_required"=>false, 
             "placeholder"=>"+23485755857",
@@ -23,4 +30,6 @@
             "email"=>"seriki@gmail.com"
         ]
     ];
+
+    // $d->create_table("staff2", $staff_from, true);
 ?>
